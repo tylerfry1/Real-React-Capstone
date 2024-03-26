cb68b1ff3e609f53aff46c36d2a3d70b04f4ddd4
tylerfry1 <tyler.fry1@outlook.com>
2024-03-26 00:19:04 -0600

    Finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import Header from './compnents/header/header';
-import Hero from './compnents/hero/hero';
-import Companies from './compnents/companies/companies';
-
+import Header from './compnents/header/header'
+import Hero from './compnents/hero/hero'
+import Companies from './compnents/companies/companies'
+import Chair from './compnents/chair/chair'
+import About from './compnents/about/about'
+import Contact from './compnents/contact/contact'
+import Footer from './compnents/footer/footer'
 const App = () => {
   return (
     <div>
      <Header/>
      <Hero/>
      <Companies/>
+     <Chair/>
+     <About/>
+     <Contact/>
+     <Footer/>
     </div>
   );
 };
---
+++ src/compnents/chair/chair.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./chair.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import 'swiper/css/scrollbar';
+import SwiperCore, { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Chairdata from "../../assets/slider.json";
+SwiperCore.use([Navigation]);
+
+const chair = () => {
+  return (
+    <div>
+      <section className="chair container" id="chair">
+        <h2 className="heading">
+          Shop<span className="gradient-text">Popular</span>
+          <br/>Categories
+        </h2>
+        <div className="chair content">
+          <Swiper
+          navigation
+          spaceBetween={15}
+          slidesPerView={1}
+          breakpoints={{
+            400:{
+              slidesPerView: 1
+            },
+            655:{
+              slidesPerView: 2
+            },
+            880:{
+              slidesPerView: 3
+            },
+          }}
+          >
+            {Chairdata.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="chair-box">
+                  <div className="box-img">
+                    <img src={item.image} alt={item.title} />
+                    <span className="box-icon">{item.icon}</span>
+                  </div>
+                  <h3 className="box-title">{item.title}</h3>
+                  <p>{item.subtitle}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default chair; 
