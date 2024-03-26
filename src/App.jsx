c99558d4805f9896d903bedb1e5aaf8c298c3f09
tylerfry1 <tyler.fry1@outlook.com>
2024-03-25 23:12:53 -0600

    Added Companies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from './compnents/header/header';
 import Hero from './compnents/hero/hero';
+import Companies from './compnents/companies/companies';
 
 const App = () => {
   return (
     <div>
      <Header/>
      <Hero/>
+     <Companies/>
     </div>
   );
 };
